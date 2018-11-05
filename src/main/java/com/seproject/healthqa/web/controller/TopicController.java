c63a34b1260c997e4e93a1e7920746420fc16fc2
Chaloemphisit Sirichai <Chaloemphisit@users.noreply.github.com>
2018-11-05 16:40:18 +0700

    update topic structure

--- src/main/java/com/seproject/healthqa/web/controller/TopicController.java
+++ src/main/java/com/seproject/healthqa/web/controller/TopicController.java
@@ -1,44 +1,54 @@
 package com.seproject.healthqa.web.controller;
 
+import com.seproject.healthqa.service.HomeService;
+import com.seproject.healthqa.service.QuestionService;
+import com.seproject.healthqa.web.bean.AllTopics;
+import com.seproject.healthqa.web.bean.Topic;
 import java.util.List;
 import java.util.logging.Logger;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
-
-import com.seproject.healthqa.service.HomeService;
-import com.seproject.healthqa.web.bean.AllTopics;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
-@RequestMapping("home")
-public class HomeController {
+@RequestMapping("topic")
+public class TopicController {
 
     private static Logger log = Logger.getLogger("InfoLogging");
 
     @Autowired
     HomeService homeService;
-    
-    @GetMapping(value = "/topics")
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping(value = "/all")
 //  @PreAuthorize("hasRole('USER')")
     public List<AllTopics> getTopics() {
         return homeService.getTopics();
     }
 
-    @GetMapping(value = "/ansTopics")
+    @GetMapping(value = "/ans")
 //  @PreAuthorize("hasRole('USER')")
     public List<AllTopics> getTopicsAns() {
         return homeService.getTopicsAns();
     }
 
-    @GetMapping(value = "/noAnsTopics")
+    @GetMapping(value = "/noAns")
 //  @PreAuthorize("hasRole('USER')")
     public List<AllTopics> getTopicsNoAns() {
         return homeService.getTopicsNoAns();
     }
-
+    
+    @GetMapping(value = "/{id}")
+//  @PreAuthorize("hasRole('USER')")
+    @ResponseBody
+    public List<Topic> getTopic(@PathVariable("id") int id_topic) {
+    	log.info(" ID_TOPIC ---------> "+id_topic);
+        return questionService.getTopic(id_topic);
+    }
 }
