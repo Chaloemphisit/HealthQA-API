6dd26738ea2556dcaf613a41dc538f4c81ccdbe7
SItthisak Thumbovorn <oatto_1997@outlook.com>
2018-11-13 16:43:50 +0700

    fix bug

--- src/main/java/com/seproject/healthqa/service/TopicService.java
+++ src/main/java/com/seproject/healthqa/service/TopicService.java
@@ -39,7 +39,7 @@ public class TopicService {
         StringBuffer queryStr = new StringBuffer("SELECT HD.HEAD_TOPIC_ID, HD.TOPIC_NAME, HD.TOPIC_TEXT, HD.WEIGHT, HD.HEIGHT, HD.AGE_Y, HD.AGE_M,"
                 + " HD.SEX, HD.DISEASE, QUESTION_PURPOSE, HD.QUESTION_TYPE, USERS.USERNAME, CREATED_DATE"
                 + " , (SELECT COUNT(*) FROM comment WHERE HEAD_TOPIC_ID=HD.HEAD_TOPIC_ID AND IS_DELETED='F')"
-                + " FROM head_topic HD LEFT JOIN users ON(users.id = HD.USER_ID)"
+                + " FROM head_topic HD LEFT JOIN users ON(users.id = HD.id)"
                 + " WHERE (HD.HEAD_TOPIC_ID = " + id_topic + ") AND (HD.IS_DELETED = 'F')");
         Topic topic = new Topic();
         Query query = entityManager.createNativeQuery(queryStr.toString());
