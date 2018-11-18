7bb9c92a0fa3dcf2e858baba724421f28002adb6
SItthisak Thumbovorn <oatto_1997@outlook.com>
2018-11-18 16:57:59 +0700

    admin controller
    mockup admin controller

---
+++ src/main/java/com/seproject/healthqa/admin/AdminController.java
@@ -0,0 +1,45 @@
+package com.seproject.healthqa.admin;
+
+import com.seproject.healthqa.web.bean.AllTopics;
+import java.util.List;
+import java.util.logging.Logger;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@CrossOrigin
+@RestController
+@RequestMapping("admin")
+public class AdminController {
+
+    private static Logger log = Logger.getLogger("InfoLogging");
+
+    @GetMapping(value = "/all")
+//  @PreAuthorize("hasRole('USER')")
+    public boolean getTopicsRp() {
+        
+        return true;
+    }
+  
+// สร้างมอคไว้เฉย ๆ     
+//    @GetMapping(value = "/delete")
+////  @PreAuthorize("hasRole('USER')")
+//    public boolean deleteDoctor() {
+//        
+//        return true;
+//    }
+//    
+//    @GetMapping(value = "/rp")
+////  @PreAuthorize("hasRole('USER')")
+//    public boolean confirmDelete() {
+//        
+//        return true;
+//    }
+    
+    
+    
+    
+}
