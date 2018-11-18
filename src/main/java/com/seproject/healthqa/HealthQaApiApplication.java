73a58aca7a2a301428d470f15b72419f618fc04a
Chaloemphisit Sirichai <Chaloemphisit@users.noreply.github.com>
2018-11-19 00:45:56 +0700

    fix timezone

--- src/main/java/com/seproject/healthqa/HealthQaApiApplication.java
+++ src/main/java/com/seproject/healthqa/HealthQaApiApplication.java
@@ -1,5 +1,9 @@
 package com.seproject.healthqa;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.TimeZone;
 import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
@@ -17,9 +21,19 @@ public class HealthQaApiApplication {
     @PostConstruct
     void init() {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Bangkok"));
+
+        Instant nowUtc = Instant.now();
+        ZoneId asiaBangkok = ZoneId.of("Asia/Bangkok");
+
+        ZonedDateTime nowAsiaSingapore = ZonedDateTime.ofInstant(nowUtc, asiaBangkok);
+
     }
 
     public static void main(String[] args) {
         SpringApplication.run(HealthQaApiApplication.class, args);
+//        System.err.println("\n\n\n\n---------------------------------------------------------->\n"
+//                + "Time Zone : " + TimeZone.getDefault() + "\n"
+//                + "" + new Date()
+//                + "\n<------------------------------------------------------------\n\n\n\n\n\n");
     }
 }
